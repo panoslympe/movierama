5e299cd22297091a9bf372b1ae32f95bfbc5d6ba
panoslymperopoulos <119874189+panoslymperopoulos@users.noreply.github.com>
2024-04-05 13:16:01 +0300

    step(2): header complete

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementById("my-element").innerHTML = "Hello World!";
+const apiKey = '745319c67fb2efc547a6b888616b2253';
+const baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+const url = baseUrl + apiKey;
+
+async function getMovies () {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+}
+
+getMovies();
