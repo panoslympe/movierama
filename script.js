db137459e854a7004120b032f2c4347bfc9a9f89
panoslymperopoulos <119874189+panoslymperopoulos@users.noreply.github.com>
2024-04-08 17:26:50 +0300

    step(4): project complete. break down methods to seperate files

--- script.js
+++ script.js
@@ -1,28 +1,82 @@
 const apiKey = "bc50218d91157b1ba4f142ef7baaa6a0";
-const baseUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
-const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
-const genreMovieList = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
-const searchUrl = "https://api.themoviedb.org/3/search/movie";
+const baseUrl = `https://api.themoviedb.org/3/`;
+const nowPlayingUrl = `${baseUrl}movie/now_playing?api_key=${apiKey}`;
+const genreUrl = `${baseUrl}genre/movie/list?api_key=${apiKey}`;
+const searchUrl = `${baseUrl}search/movie`;
+const searchMovieUrl = `${baseUrl}movie`;
 
-const getImage = (image) => `https://image.tmdb.org/t/p/original/${image}`;
+const getImage = (image) => {
+  return image
+    ? `https://image.tmdb.org/t/p/original/${image}`
+    : "./assets/icons/noImage.png";
+};
 
 const searchbox = document.getElementById("query");
+const modal = document.querySelector(".modal");
+const mainContainer = document.querySelector(".main-container");
+const closeModalBtn = document.querySelector(".close-modal");
+const moviesContainer = document.querySelector(".movies");
+const moviesTitle = document.querySelector(".movies-title");
+moviesTitle.innerHTML = "Playing Now";
 
 const movieListContainer = document.querySelector(".movie-list-container");
 
-//single-source of truth
+const createYouTubeIframe = (videoId) => {
+  const iframe = document.createElement("iframe");
+
+  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+
+  iframe.frameborder = 0;
+
+  iframe.allowfullscreen = true;
+  iframe.width = 640;
+  iframe.height = 390;
+
+  return iframe;
+};
+
+//single source of truth
 const state = {
   genres: {},
   movies: [],
   searchMovies: [],
+  clickedMovie: null,
 };
 
 const fetchGenres = async () => {
-  const response = await fetch(genreMovieList);
+  const response = await fetch(genreUrl);
   const data = await response.json();
   const returnVal = {};
   data.genres.forEach(({ id, name }) => (returnVal[id] = name));
   state.genres = returnVal;
+};
+
+const fetchMovie = async (id) => {
+  const movieRes = await fetch(`${searchMovieUrl}/${id}?api_key=${apiKey}`);
+  const movieData = await movieRes.json();
+
+  const movieVideoRes = await fetch(
+    `${searchMovieUrl}/${id}/videos?api_key=${apiKey}`
+  );
+  const videoData = await movieVideoRes.json();
+
+  const reviewsRes = await fetch(
+    `${searchMovieUrl}/${id}/reviews?api_key=${apiKey}`
+  );
+  const reviewsData = await reviewsRes.json();
+
+  const similarsRes = await fetch(
+    `${searchMovieUrl}/${id}/similar?api_key=${apiKey}`
+  );
+  const similarData = await similarsRes.json();
+
+  const videoTrailer = videoData.results.find(
+    (result) => result.type === "Trailer"
+  );
+  const reviews = reviewsData.results.slice(0, 2);
+  const similar = similarData.results.slice(0, 7);
+
+  return { movie: movieData, trailer: videoTrailer, reviews, similar };
 };
 
 const fetchMovies = async () => {
@@ -41,7 +95,7 @@ const fetchMovies = async () => {
       { page: data.page, movieList: data.results },
     ];
   } else {
-    response = await fetch(baseUrl + `&page=${state.movies.length + 1}`);
+    response = await fetch(nowPlayingUrl + `&page=${state.movies.length + 1}`);
     const data = await response.json();
     state.movies = [
       ...state.movies,
@@ -65,8 +119,7 @@ const renderMovies = () => {
   });
 };
 
-// Function to create the movie component element
-function createMovieElement(movie) {
+const createMovieElement = (movie) => {
   const movieContainer = document.createElement("div");
   movieContainer.classList.add("movie-container");
 
@@ -113,16 +166,121 @@ function createMovieElement(movie) {
   movieContainer.appendChild(poster);
   movieContainer.appendChild(movieInfo);
 
+  movieContainer.addEventListener("click", () => {
+    handleModal(movie.id);
+  });
   return movieContainer;
-}
+};
 
-// and alternative choice to the bellow function was for me to implement Intersection Observer API but I chose that one
-function isScrolledToBottom() {
+const handleModal = async (movieId) => {
+  const movieData = await fetchMovie(movieId);
+  createModal(movieData);
+};
+
+const createModal = (movieData) => {
+  const modalOld = document.querySelector(".modal");
+  if (modalOld) {
+    modalOld.remove();
+  }
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const modalInfo = document.createElement("div");
+  modalInfo.classList.add("modal-info");
+
+  const modalInfoTitle = document.createElement("div");
+  modalInfoTitle.classList.add("modal-info-title");
+
+  modalInfoTitle.innerHTML = movieData.movie.title;
+
+  const modalHero = document.createElement("div");
+  modalHero.classList.add("modal-info-hero");
+
+  const video = document.createElement("div");
+  video.classList.add("video");
+  if (movieData.trailer?.key) {
+    video.appendChild(createYouTubeIframe(movieData.trailer.key));
+  } else {
+    const noVideo = document.createElement("img");
+    noVideo.classList.add("no-video");
+    noVideo.src = "./assets/icons/noVideo.png";
+    video.appendChild(noVideo);
+  }
+
+  const reviews = document.createElement("div");
+  const reviewsTitle = document.createElement("h2");
+  reviewsTitle.innerHTML = "Reviews";
+  reviews.appendChild(reviewsTitle);
+  reviews.classList.add("reviews");
+
+  if (movieData.reviews.length) {
+    movieData.reviews.forEach((review) => {
+      const reviewContainer = document.createElement("div");
+      reviewContainer.classList.add("review-container");
+      const author = document.createElement("div");
+      author.classList.add("author");
+      author.innerHTML = review.author;
+      const reviewContent = document.createElement("div");
+      reviewContent.classList.add("review-content");
+      reviewContent.innerHTML = review.content;
+      reviewContainer.appendChild(author);
+      reviewContainer.appendChild(reviewContent);
+      reviews.appendChild(reviewContainer);
+    });
+  } else {
+    const noReviewsAvailable = document.createElement("div");
+    noReviewsAvailable.classList.add("no-reviews-available");
+    noReviewsAvailable.innerHTML = "No reviews avaiable";
+    reviews.appendChild(noReviewsAvailable);
+  }
+
+  modalHero.appendChild(video);
+  modalHero.appendChild(reviews);
+
+  const similar = document.createElement("div");
+  similar.classList.add("similar");
+
+  const similarTitle = document.createElement("h2");
+  similarTitle.classList.add("similar-title");
+  similarTitle.innerHTML = "Similar";
+
+  similar.appendChild(similarTitle);
+
+  const similarContainer = document.createElement("div");
+  similarContainer.classList.add("similar-container");
+
+  similar.appendChild(similarContainer);
+
+  movieData.similar.forEach((item) => {
+    const movieContainer = createMovieElement(item);
+    similarContainer.appendChild(movieContainer);
+  });
+
+  modalInfo.appendChild(modalInfoTitle);
+  modalInfo.appendChild(modalHero);
+  modalInfo.appendChild(similar);
+
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("close-modal");
+  closeButton.textContent = "X";
+  closeButton.addEventListener("click", () => {
+    modal.remove();
+  });
+
+  modal.appendChild(modalInfo);
+  modal.appendChild(closeButton);
+
+  mainContainer.appendChild(modal);
+
+  return modal;
+};
+
+const isScrolledToBottom = () => {
   const scrolledPosition = window.scrollY + window.innerHeight;
   const scrollHeight = document.body.scrollHeight;
 
   return scrolledPosition >= scrollHeight;
-}
+};
 
 window.addEventListener("scroll", () => {
   const isAtBottom = isScrolledToBottom();
@@ -145,13 +303,13 @@ const throttle = (func, delay) => {
   };
 };
 
-function debounce(func, delay) {
+const debounce = (func, delay) => {
   let timeout;
   return (...args) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.apply(this, args), delay);
   };
-}
+};
 
 const resetState = () => {
   state.movies = [];
@@ -165,15 +323,7 @@ searchbox.addEventListener("input", (event) => {
   const searchTerm = event.target.value;
   movieListContainer.innerHTML = "";
   resetState();
-
-  debounce(fetchMovies, 1000)();
-});
-
-searchbox.addEventListener("input", (event) => {
-  event.preventDefault();
-  const searchTerm = event.target.value;
-  movieListContainer.innerHTML = "";
-  resetState();
+  searchTerm ? moviesTitle.innerHTML = "Search results:" : moviesTitle.innerHTML = "Playing Now";
 
   debounce(fetchMovies, 1000)();
 });
